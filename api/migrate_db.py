630975dd8f452214d29851c5fcb2a9c0f2e6cf6a
sushi-chaaaan <sushi_code@outlook.jp>
2022-10-15 14:45:21 +0900

    DBの周りのセットアップ

---
+++ api/migrate_db.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+
+from api.models.feed import Feed
+
+DB_URL = "mysql+pymysql://root@localhost:33306/demo?charset=utf8"
+engine = create_engine(DB_URL, echo=True)
+
+
+def reset_database():
+    Feed.metadata.drop_all(engine)
+    Feed.metadata.create_all(engine)
+
+
+if __name__ == "__main__":
+    reset_database()
